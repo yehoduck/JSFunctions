e81f4dec3a486eb2edd9274e8d9bfea77ef8f5e4
yehoduck <ehorkovalenko111@gmail.com>
2026-08-19 07:42:55 +0300

    Add function calcPaint&validation

--- script3.js
+++ script3.js
@@ -9,7 +9,6 @@ function calculateSurfaceArea(shapeType, param1, param2) {
         default: 
             return 0
     }
-
 }
 console.group("Calculate surface area")
 console.group("Cube surface area")
@@ -22,4 +21,15 @@ console.log(calculateSurfaceArea("cylinder", 3, 7))
 console.log(calculateSurfaceArea("cylinder", 2, -5))
 console.log(calculateSurfaceArea("cylinder", "acdc", 4))
 console.groupEnd()
+console.groupEnd()
+
+function calculatePaintBoxes(area, layersCount = 1) {
+    if (area < 0 || layersCount < 0 || typeof area !== "number" || typeof layersCount !== "number") return 0
+    const paint = area * layersCount * 0.1;
+    return Math.ceil(paint);
+}
+console.group("Calculate paint boxes")
+console.log(calculatePaintBoxes(20, 5))
+console.log(calculatePaintBoxes("metallica", 10))
+console.log(calculatePaintBoxes(25, -15))
 console.groupEnd()
